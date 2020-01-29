cffb5107933271c751e4bff9ce1891e1404af132
Benoit Suttor <bsuttor@imio.be>
2020-01-29 09:32:26 +0100

    Add test for faceted-map-geojson-popup view

--- src/imioweb/core/browser/view.py
+++ src/imioweb/core/browser/view.py
@@ -4,11 +4,11 @@ from Products.Five import BrowserView
 
 
 class FacetedGeoJSONPopup(BrowserView):
-    def popup(self, brain):
+    def popup(self, brain, short_names=["applications"]):
         template = "<a href='{0}' title='{1}'>" "<div>{1} <ul>{2}</ul></div></a>"
         obj = brain.getObject()
         tax_view = TaxonomyView(obj, obj.REQUEST)
-        taxonomies = tax_view.taxonomiesForContext(short_names=["applications"])
+        taxonomies = tax_view.taxonomiesForContext(short_names=short_names)
         taxonomies.sort()
         taxonomy = "".join(["<li>{}</li>".format(tax) for tax in taxonomies])
         return template.format(brain.getURL(), brain.Title, taxonomy)
--- src/imioweb/core/testing.py
+++ src/imioweb/core/testing.py
@@ -19,33 +19,28 @@ class ImiowebCoreLayer(PloneSandboxLayer):
         # The z3c.autoinclude feature is disabled in the Plone fixture base
         # layer.
         import plone.restapi
+
         self.loadZCML(package=plone.restapi)
-        self.loadZCML(package=imioweb.core)
+        self.loadZCML(name="testing.zcml", package=imioweb.core)
 
     def setUpPloneSite(self, portal):
-        applyProfile(portal, 'imioweb.core:default')
+        applyProfile(portal, "imioweb.core:default")
 
 
 IMIOWEB_CORE_FIXTURE = ImiowebCoreLayer()
 
 
 IMIOWEB_CORE_INTEGRATION_TESTING = IntegrationTesting(
-    bases=(IMIOWEB_CORE_FIXTURE,),
-    name='ImiowebCoreLayer:IntegrationTesting',
+    bases=(IMIOWEB_CORE_FIXTURE,), name="ImiowebCoreLayer:IntegrationTesting"
 )
 
 
 IMIOWEB_CORE_FUNCTIONAL_TESTING = FunctionalTesting(
-    bases=(IMIOWEB_CORE_FIXTURE,),
-    name='ImiowebCoreLayer:FunctionalTesting',
+    bases=(IMIOWEB_CORE_FIXTURE,), name="ImiowebCoreLayer:FunctionalTesting"
 )
 
 
 IMIOWEB_CORE_ACCEPTANCE_TESTING = FunctionalTesting(
-    bases=(
-        IMIOWEB_CORE_FIXTURE,
-        REMOTE_LIBRARY_BUNDLE_FIXTURE,
-        z2.ZSERVER_FIXTURE,
-    ),
-    name='ImiowebCoreLayer:AcceptanceTesting',
+    bases=(IMIOWEB_CORE_FIXTURE, REMOTE_LIBRARY_BUNDLE_FIXTURE, z2.ZSERVER_FIXTURE),
+    name="ImiowebCoreLayer:AcceptanceTesting",
 )
