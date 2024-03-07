931ae6071661f311dc7802a664215703e0fdf81b
cboulanger <christophe.boulanger@imio.be>
2024-03-07 11:21:00 +0100

    Back to development: 1.0a5

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="imioweb.core",
-    version='1.0a4',
+    version='1.0a5.dev0',
     description="Core package for imioweb",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
